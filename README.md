e8d93c335b7c4be1c9f5f8fd0768ba5bc690e2f7
LucasMartinsUthi <luscagbr@gmail.com>
2022-07-20 00:10:22 -0300

    first-commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# fbot-vss-rust
+Rust version
+
+
+## Executando Exemplos
+Exemplos são executados no robô amarelo de id 0
+- Seguidor de Bola: `cargo run --example follow_ball`
---
+++ src/lib.rs
@@ -0,0 +1,108 @@
+
+use std::io::Cursor;
+use prost::Message;
+use std::net::UdpSocket;
+
+pub mod fira_protos;
+
+const VISION_ADDRS: &str = "224.0.0.1:10002";
+const COMMAND_ADDRS: &str = "127.0.0.1:20011";
+
+#[derive(Debug)]
+pub enum Team{
+    Yellow,
+    Blue
+}
+
+pub fn serialize_packet(packet: &fira_protos::Packet) -> Vec<u8> {
+    let mut buf = Vec::new();
+    buf.reserve(packet.encoded_len());
+    
+    // Unwrap is safe, since we have reserved sufficient capacity in the vector.
+    packet.encode(&mut buf).unwrap();
+    buf
+}
+
+pub fn deserialize_env(buf: &[u8]) -> Result<fira_protos::Environment, prost::DecodeError> {
+    fira_protos::Environment::decode(&mut Cursor::new(buf))
+}
+
+pub fn send_command(commands: fira_protos::Commands) {
+    let socket_sender = UdpSocket::bind(VISION_ADDRS).unwrap();
+
+    let packet = fira_protos::Packet {
+        cmd: Some(commands),
+        replace: None        
+    };
+    let buf = serialize_packet(&packet); 
+
+    match socket_sender.send_to(&buf, COMMAND_ADDRS) {
+        Ok(_) => {},
+        Err(e) => {
+            println!("Error Send {}", e)
+        }
+    };
+}
+
+fn frame() -> Option<fira_protos::Frame>{
+    let socket = UdpSocket::bind(VISION_ADDRS).unwrap();
+    let mut buf = [0; 1024];
+
+    let (len, _) = socket.recv_from(&mut buf).unwrap();
+
+    let env = deserialize_env(&buf[..len]).unwrap();
+
+    env.frame
+}
+
+pub fn ball() -> fira_protos::Ball {
+    let mut ret = fira_protos::Ball{
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+        vx: 0.0,
+        vy: 0.0,
+        vz: 0.0,
+    };
+
+    if let Some(frame) = frame() {
+        if let Some(ball) = frame.ball {
+            ret = ball
+        }
+    }
+
+    ret
+}
+
+pub fn blue_robot(id: &u32) -> Option<fira_protos::Robot> {
+    if let Some(frame) = frame() {
+        let mut ret = None;
+
+        for robot in frame.robots_blue {
+            if robot.robot_id == *id {
+                ret = Some(robot)
+            }
+        };
+
+        ret
+
+    } else { None }
+}
+
+pub fn yellow_robot(id: &u32) -> Option<fira_protos::Robot> {
+    if let Some(frame) = frame() {
+        let mut ret = None;
+
+        for robot in frame.robots_yellow {
+            if robot.robot_id == *id {
+                ret = Some(robot)
+            }
+        };
+
+        ret
+
+    } else { 
+        None 
+    }
+}
+
